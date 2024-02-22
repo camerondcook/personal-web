b93e73dc88af119f9940c21e31603794841e5897
Cameron Cook <camerondcook10@gmail.com>
2024-02-22 15:52:35 -0600

    Added Tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ContactPageComponent } from './Components/contact/contact-page.component';
+import { PageNotFoundComponent } from './Components/notfound/page-not-found.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { BlogComponent } from './Components/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', title: 'Home', component: HomeComponent},
+    {path: 'projects', title: 'Projects', component: ProjectsComponent },
+    {path: 'blog', title: 'Blog', component: BlogComponent },
+    {path: 'contact', title: 'Contact', component: ContactPageComponent },
+    {path: '**', component: PageNotFoundComponent}
+];
